a47a97f2f4a8076fa717186bde7c0d825e7c13e4
Anton Khomchenko <anton@khomchenko.com>
2023-11-15 17:04:22 +0000

    clean: delete outdated folder

--- __backup/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-    <main className="flex w-full justify-center items-center align-middle">
-      <h1 className="text-center">HOME PAGE</h1>
-    </main>
-  );
-}
