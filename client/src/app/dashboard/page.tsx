0e9688d1449b15fa2725935b1742b598ac3b71c2
Anton Khomchenko <khomchenko.anton@gmail.com>
2024-01-05 11:52:22 +0000

    fix: paste same url twice (if first parsing failed)

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -69,6 +69,7 @@ export default function Dashboard() {
           dispatch(handleAddNewJobToColumn(res));
           dispatch(addNewJob(res));
         });
+      setUrlToParse('');
     }
   }, [urlToParse]);
 
